448ee603b0e280713370a186205f2754651bf782
Kamil Górski <kamil@freakone.pl>
2016-04-12 13:50:06 +0200

    mocked charts

--- joule-fe/src/components/current-parameters.vue
+++ joule-fe/src/components/current-parameters.vue
@@ -5,38 +5,69 @@
         .box.box-primary
           .box-header.with-border
             h3.box-title Temperatura 1
+          .box-body
+            vue-chart(:chart-type="LineChart", :columns="columns", :rows="rows", :options="options")
 
         .box.box-primary
           .box-header.with-border
             h3.box-title Temperatura 2
+          .box-body
+            vue-chart(:chart-type="LineChart", :columns="columns", :rows="rows", :options="options")
 
       .col-md-6
         .box.box-primary
           .box-header.with-border
             h3.box-title Temperatura 3
+          .box-body
+            vue-chart(:chart-type="LineChart", :columns="columns", :rows="rows", :options="options")
 
         .box.box-primary
           .box-header.with-border
             h3.box-title Temperatura 4
+          .box-body
+            vue-chart(:chart-type="LineChart", :columns="columns", :rows="rows", :options="options")
     .col-md-2
       | BOK
-
-
 </template>
 
 <script>
 export default {
   data () {
     return {
-      msg: 'Hello xDxDxD!'
+      columns: [{
+        'type': 'string',
+        'label': 'Year'
+      }, {
+        'type': 'number',
+        'label': 'Sales'
+      }, {
+        'type': 'number',
+        'label': 'Expenses'
+      }],
+      rows: [
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540]
+      ],
+      options: {
+        hAxis: {
+          title: 'Year',
+          minValue: '2004',
+          maxValue: '2007'
+        },
+        vAxis: {
+          title: '',
+          minValue: 300,
+          maxValue: 1200
+        },
+        height: 300,
+        curveType: 'function'
+      }
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  color: #42b983;
-}
 </style>
--- joule-fe/src/main.js
+++ joule-fe/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import VueCharts from 'vue-charts'
 
 require('expose?$!expose?jQuery!jquery')
 require('jquery-ui')
 require('assets/bootstrap/js/bootstrap.min.js')
 require('assets/admin-lte/js/app.js')
 
+Vue.use(VueCharts)
 /*eslint-disable */
 new Vue({
   el: 'body',
